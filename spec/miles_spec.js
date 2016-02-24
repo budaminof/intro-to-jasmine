5f1325ebc101827ba132d73ece4e5e2841f54664
bud aminof <budaminof@gmail.com>
2016-02-24 11:52:45 -0700

    wrote a function that works for both expectation

--- spec/miles_spec.js
+++ spec/miles_spec.js
@@ -1,7 +1,21 @@
 var miles = require('../lib/miles.js');
 
 describe('miles', function () {
-  it('is our master plan here', function () {
-    expect(miles.someFunction()).toEqual('running stuff foreal');
+  it('determines if a car has enough gas to travel route', function () {
+    var car = {
+      milesPerGalon: 20,
+      gallonsInTank: 3
+    };
+
+    var route = {
+      miles: 200,
+      destination: "Boulder",
+      location: "Fancy Land"
+    };
+
+    expect(miles.hasEnoughtGas(car, route)).toEqual(false);
+    car.gallonsInTank += 20;
+    expect(miles.hasEnoughtGas(car, route)).toEqual(true);
+
   })
 })
